798feef6a6152829e7884351c2168355a56025ba
Shadowpigkiller <119260016+Shadowpigkiller@users.noreply.github.com>
2025-10-31 06:33:13 -0400

    THE GAME IS BUILT AHAHAHAHAHAHAHAH

--- Assets/Systems/Attack System/Scripts/OverallCountDownTimer.cs
+++ Assets/Systems/Attack System/Scripts/OverallCountDownTimer.cs
@@ -1,13 +1,26 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class OverallCountDownTimer : MonoBehaviour
 {
     public TextMeshProUGUI winTimer;
     private float timer = 180f;
+    [SerializeField] AudioSource backgroundMusic;
+    [SerializeField] AudioClip stage1;
+    [SerializeField] AudioClip stage2;
+    [SerializeField] AudioClip stage3;
+    private bool stage2Start = true;
+    private bool stage3Start = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        backgroundMusic.Stop();
+        backgroundMusic.clip = stage1;
+        backgroundMusic.Play();
 
+    }
     // Update is called once per frame
     void Update()
     {
@@ -21,8 +34,22 @@ public class OverallCountDownTimer : MonoBehaviour
             gameObject.GetComponent<WinLoseScript>().ShowWinScreen(true);
         }
         else
-        { 
+        {
             timer -= Time.deltaTime;
+        }
+        if (stage3Start && timer <= 60)
+        {
+            stage3Start = false;
+            backgroundMusic.Stop();
+            backgroundMusic.clip = stage3;
+            backgroundMusic.Play();
+        }
+        else if (stage2Start && timer <= 120)
+        {
+            stage2Start = false;
+            backgroundMusic.Stop();
+            backgroundMusic.clip = stage2;
+            backgroundMusic.Play();
         }
     }
 }
